660e593435a2647dc8e8a3aa19c07ec916d5c8bf
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-06-13 18:35:29 +0300

    add final food collection to temp

---
+++ temp/recipes.js
@@ -0,0 +1,21 @@
+// const fs = require("fs");
+// const { array } = require("./resArr");
+
+// const result = array.filter((item) => item.category === "Dessert");
+
+// fs.appendFile("hello.json", JSON.stringify(result), function (error) {
+//   if (error) throw error; // если возникла ошибка
+// });
+
+// const mainDish = require("./mainFoodBase.json");
+
+// const result = mainDish.map((obj) => ({
+//   ...obj,
+//   price: (Math.random() * (45 - 20) + 20).toString().slice(0, 5),
+// }));
+
+// console.log(result);
+
+// fs.appendFile("foodCollection.json", JSON.stringify(result), function (error) {
+//   if (error) throw error; // если возникла ошибка
+// });
