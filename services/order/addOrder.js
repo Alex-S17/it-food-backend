27f38ed66b0b8f9e67af64ffb65f782ee68344f3
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-07-02 20:18:16 +0300

    fix get last order logic

--- services/order/addOrder.js
+++ services/order/addOrder.js
@@ -1,14 +1,19 @@
 const { Order } = require("../../models/orderModel");
 
 const addOrder = async (req) => {
-  const { note, option, dishes, phone, customerName } = req.body;
+  const { note, option, orderedDish, phone, customerName } = req.body;
+  console.log("addOrder => orderedDish:", orderedDish);
+
   const user = req.user;
 
+  const orderCount = await Order.count({});
+
   return await Order.create({
+    orderNumber: String(orderCount + 1).padStart(6, "0"),
     customerName,
     note,
     option,
-    dishes,
+    orderedDish,
     owner: user ? user._id : null,
     phone,
   });
--- services/order/getLastOrder.js
+++ services/order/getLastOrder.js
@@ -1,16 +1,72 @@
 const { NonExistingParamsError } = require("../../helpers/errors");
 const { Order } = require("../../models/orderModel");
+const ObjectId = require("mongodb").ObjectId;
 
 const getLastOrder = async (req) => {
-  const owner = req.user?._id;
+  const userId = req.user?._id;
 
   const phone = req.body?.phone;
 
-  console.log(req.body);
+  const owner = new ObjectId(userId);
 
-  if (!phone && !owner) throw new NonExistingParamsError("Credentials error");
+  if (!phone && !userId) throw new NonExistingParamsError("Credentials error");
 
-  return Order.findOne({ owner, phone }).sort({ _id: -1 });
+  const result = await Order.aggregate([
+    {
+      $match: {
+        $and: [
+          {
+            $or: [{ owner }, { phone }],
+          },
+        ],
+      },
+    },
+    { $sort: { _id: -1 } },
+    { $limit: 1 },
+    {
+      $lookup: {
+        from: "dishes",
+        localField: "dishes._id",
+        foreignField: "id",
+        as: "dishesData",
+      },
+    },
+    {
+      $set: {
+        orderedDish: {
+          $map: {
+            input: "$orderedDish",
+            in: {
+              $mergeObjects: [
+                "$$this",
+                {
+                  $arrayElemAt: [
+                    "$dishesData",
+                    {
+                      $indexOfArray: ["$dishesData._id", "$$this.id"],
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+
+    {
+      $unset: [
+        "dishesData",
+        "orderedDish.ingredients",
+        "orderedDish.id",
+        "orderedDish.instructions",
+      ],
+    },
+  ]);
+
+  return result;
+
+  // return Order.findOne({ owner, phone }).sort({ _id: -1 });
 };
 
 module.exports = { getLastOrder };
