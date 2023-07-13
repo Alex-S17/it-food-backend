0ce16ca3da616a08512acac0339c90f89ad98326
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-07-13 21:36:33 +0300

    fix get user response

--- services/order/getUserOrder.js
+++ services/order/getUserOrder.js
@@ -1,9 +1,58 @@
+const ObjectId = require("mongodb").ObjectId;
 const { Order } = require("../../models/orderModel");
 
 const getUserOrder = async (req) => {
   const { _id: owner } = req.user;
 
-  return await Order.find({ owner });
+  const ownerId = new ObjectId(owner);
+
+  const result = await Order.aggregate([
+    {
+      $match: { owner: ownerId },
+    },
+
+    {
+      $lookup: {
+        from: "dishes",
+        localField: "dishes._id",
+        foreignField: "id",
+        as: "dishesData",
+      },
+    },
+    {
+      $set: {
+        orderedDish: {
+          $map: {
+            input: "$orderedDish",
+            in: {
+              $mergeObjects: [
+                "$$this",
+                {
+                  $arrayElemAt: [
+                    "$dishesData",
+                    {
+                      $indexOfArray: ["$dishesData._id", "$$this.id"],
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+
+    {
+      $unset: [
+        "dishesData",
+        "orderedDish.ingredients",
+        "orderedDish.id",
+        "orderedDish.instructions",
+      ],
+    },
+  ]);
+
+  return result;
 };
 
 module.exports = { getUserOrder };
